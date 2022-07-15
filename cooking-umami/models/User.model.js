ee7ebcbe9c886be2ab5d83f9175f63a82312a754
Darren Iyoyo <darreniyoyo@gmail.com>
2022-07-15 12:31:16 +0200

    model added

--- cooking-umami/models/User.model.js
+++ cooking-umami/models/User.model.js
@@ -1,20 +1,23 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    email: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      required: [true, "Email is required."],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    password: String,
+    passwordHash: {
+      type: String,
+      required: [true, "Password is required."],
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
 
-const User = model("User", userSchema);
-
-module.exports = User;
+module.exports = model("User", userSchema);
