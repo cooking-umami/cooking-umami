cd3ef5ab261bf940dc907663d6c0793ece5c9542
jackletran <jack.letran@moebel.de>
2022-07-19 14:49:11 +0200

    fix seed img relative path

--- cooking-umami/bin/seeds.js
+++ cooking-umami/bin/seeds.js
@@ -5,56 +5,57 @@ const User = require("../models/User.model");
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://localhost/cooking-umami";
 
-  
-  const users = [
-    {
-      userName: "darren",
-      passwordHash: "123",
-      nationality: "Dutch",
-      aboutMe: "Oranje for life!",
-      gender: "male",
-    },
-    {
-        userName: "jack",
-        passwordHash: "123",
-        nationality: "Vietnamese",
-        aboutMe: "Cooking on Mama-level.",
-        gender: "male",
-    },
-      {
-        userName: "cookingmama",
-        passwordHash: "123",
-        nationality: "Japanese",
-        aboutMe: "I made this.",
-        gender: "female",
-      },
-      
-]
+const users = [
+  {
+    userName: "darren",
+    passwordHash: "123",
+    nationality: "Dutch",
+    aboutMe: "Oranje for life!",
+    gender: "male",
+  },
+  {
+    userName: "jack",
+    passwordHash: "123",
+    nationality: "Vietnamese",
+    aboutMe: "Cooking on Mama-level.",
+    gender: "male",
+  },
+  {
+    userName: "cookingmama",
+    passwordHash: "123",
+    nationality: "Japanese",
+    aboutMe: "I made this.",
+    gender: "female",
+  },
+];
 
-  const recipes = [
-    {
+const recipes = [
+  {
     title: "Garlic Shrimp",
     ingredients: "Shrimp, Red Chili, Salt",
-    description: "A classic local Hawaiian dish consisting of shrimp pan-fried with plenty of garlic and butter, it's a very popular item amongst Hawaiian food trucks.",
-    instructions: "1. Clean the shrimp!. 2. Dress the ingredients!  3. Coat it!",
+    description:
+      "A classic local Hawaiian dish consisting of shrimp pan-fried with plenty of garlic and butter, it's a very popular item amongst Hawaiian food trucks.",
+    instructions:
+      "1. Clean the shrimp!. 2. Dress the ingredients!  3. Coat it!",
     dishType: "lunch",
-    image: "./images/garlic-shrimp.webp",
-    duration : 15,
+    image: "/images/garlic-shrimp.webp",
+    duration: 15,
     creator: "",
     rating: 5,
-    review: "The best way to make shrimp in my opinion."
+    review: "The best way to make shrimp in my opinion.",
   },
   {
     title: "Lasagna",
     ingredients: "Pork, Garlic, Onion",
-    description: "A pasta dish known for its layers of meat, cheese, and sauce. Lasagna is also used to refer to the wide, flat pasta used in the dish.",
+    description:
+      "A pasta dish known for its layers of meat, cheese, and sauce. Lasagna is also used to refer to the wide, flat pasta used in the dish.",
     instructions: "1. Cut the Ingredients! 2. Stir Fry it! 3. Stir it!",
     dishType: "dinner",
     image: "/images/lasagna.webp",
-    duration : 80,
+    duration: 80,
     creator: "",
     rating: 3,
-    review: "Easy recipe, but Italians would not approve."
+    review: "Easy recipe, but Italians would not approve.",
   },
   {
     title: "Popcorn",
@@ -63,14 +64,12 @@ const MONGO_URI =
     instructions: "1. make some popcorn! 2. Add some seasoning!",
     dishType: "snack",
     image: "/images/popcorn.webp",
-    duration : 15,
+    duration: 15,
     creator: "",
     rating: 4,
-    review: "Be careful not to burn the kernels!"
+    review: "Be careful not to burn the kernels!",
   },
 ];
-
-
 
 mongoose
   .connect(MONGO_URI)
@@ -82,32 +81,30 @@ mongoose
     Recipe.collection.drop(); // Warning, drops recipe collection :)
     User.collection.drop(); // Warning, drops user collection :)
 
-    return User.create(users)
+    return User.create(users);
   })
   .then((result) => {
     const usersCreated = result;
     console.log(`Number of users created... ${usersCreated.length} `);
 
-    for (let i = 0; i<recipes.length; i++){
-      if(i<1){
-        recipes[i].creator = usersCreated[0]._id
-      } else if(i<2){
-        recipes[i].creator = usersCreated[1]._id
-      }else{
-        recipes[i].creator = usersCreated[2]._id
+    for (let i = 0; i < recipes.length; i++) {
+      if (i < 1) {
+        recipes[i].creator = usersCreated[0]._id;
+      } else if (i < 2) {
+        recipes[i].creator = usersCreated[1]._id;
+      } else {
+        recipes[i].creator = usersCreated[2]._id;
       }
     }
     return Recipe.create(recipes);
   })
-  .then((result)=> {
+  .then((result) => {
     const recipesCreated = result;
     console.log(`Number of recipes created... ${recipesCreated.length} `);
 
-        // Once created, close the DB connection
-        mongoose.connection.close();
+    // Once created, close the DB connection
+    mongoose.connection.close();
   })
   .catch((err) => {
     console.log(`An error occurred while creating recipes from the DB: ${err}`);
   });
-
-
